01f87d9293ef2d6e8e9d0b2a6afa1acc140022f8
Patrick Cura <patrick.cura@soit.local>
2016-04-07 11:17:50 +1200

    A checkin to my application

--- project.js
+++ project.js
@@ -4,44 +4,64 @@
  * and open the template in the editor.
  */
 
-/** 
-http://stackoverflow.com/questions/3502354/how-to-check-if-a-textbox-is-empty-using-javascript 
-http://www.tutorialspoint.com/javascript/javascript_form_validations.htm
-http://stackoverflow.com/questions/21822324/javascript-form-validation-without-alert
-http://www.w3schools.com/jsref/jsref_regexp_wordchar.asp
-http://www.w3schools.com/jsref/jsref_regexp_g.asp
- 
- **/
+/**
+ * http://stackoverflow.com/questions/3502354/how-to-check-if-a-textbox-is-empty-using-javascript
+ * http://www.tutorialspoint.com/javascript/javascript_form_validations.htm
+ * http://stackoverflow.com/questions/21822324/javascript-form-validation-without-alert
+ * http://www.w3schools.com/jsref/jsref_regexp_wordchar.asp
+ * http://www.w3schools.com/jsref/jsref_regexp_g.asp
+ * http://www.w3resource.com/javascript/form/phone-no-validation.php
+ */
 
-function validateFields() {
-    var illegalChars = /\W/; // allow letters, numbers, and underscores
+function validateFields()
+{
+	var illegalChars = /\W/; // allow letters, numbers, and underscores
 
-    var firstName = document.getElementById('firstName');
-    var lastName = document.getElementById('lastName');
-    var address = document.getElementById('address');
-    var phoneNumber = document.getElementById('phoneNumber');
-    var emailAddress = document.getElementById('emailAddress');
-    var username = document.getElementById('username');
-    var password = document.getElementById('password');
+	var firstName = document.getElementById('firstName');
+	var lastName = document.getElementById('lastName');
+	var address = document.getElementById('address');
+	var phoneNumber = document.getElementById('phoneNumber');
+	var emailAddress = document.getElementById('emailAddress');
+	var username = document.getElementById('username');
+	var password = document.getElementById('password');
 
-    if (firstName.value == "") 
-    {
-        alert(firstName.getAttribute("name")+ " is required" );
-        return false;
-    } 
-    else if ((fld.value.length < 5) || (fld.value.length > 15)) 
-    {
-        fld.style.background = 'Yellow';
-        error = "The username is the wrong length.\n";
-    } 
-    else if (illegalChars.test(fld.value)) 
-    {
-        fld.style.background = 'Yellow';
-        error = "The username contains illegal characters.\n";
-    } 
-    else 
-    {
-        fld.style.background = 'White';
-    }
-    return true;
+	if (!validateRequired(firstName)) {return false;}
+        if (!validateRequired(lastName)) {return false;}
+        if (!validateRequired(address)) {return false;}
+        if (!validateRequired(phoneNumber)) {return false;}
+        if (!validateRequired(emailAddress)) {return false;}
+        if (!validateRequired(username)) {return false;}
+        if (!validateRequired(password)) {return false;}
+        
+
+
+	if ((fld.value.length < 5) || (fld.value.length > 15))
+	{
+		fld.style.background = 'Yellow';
+		error = "The username is the wrong length.\n";
+	}
+	else if (illegalChars.test(fld.value))
+	{
+		fld.style.background = 'Yellow';
+		error = "The username contains illegal characters.\n";
+	}
+	else
+	{
+		fld.style.background = 'White';
+	}
+	
+        return true;
+}
+
+function validateRequired(field)
+{
+	if (field.value == "")
+	{
+		alert(field.getAttribute("name") + " is required");
+		return false;
+	}
+        else
+        {
+            return true;
+        }
 }
