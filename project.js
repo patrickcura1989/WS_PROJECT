30c7f8e8a24e0b5cf85e25fe2b346d4231d2705b
Patrick Cura <patrick.cura@soit.local>
2016-04-07 12:38:43 +1200

    A checkin to my application

--- project.js
+++ project.js
@@ -11,57 +11,107 @@
  * http://www.w3schools.com/jsref/jsref_regexp_wordchar.asp
  * http://www.w3schools.com/jsref/jsref_regexp_g.asp
  * http://www.w3resource.com/javascript/form/phone-no-validation.php
+ * http://www.regexpal.com/
  */
 
 function validateFields()
 {
-	var illegalChars = /\W/; // allow letters, numbers, and underscores
+    //var illegalChars = /\W/; // allow letters, numbers, and underscores
 
-	var firstName = document.getElementById('firstName');
-	var lastName = document.getElementById('lastName');
-	var address = document.getElementById('address');
-	var phoneNumber = document.getElementById('phoneNumber');
-	var emailAddress = document.getElementById('emailAddress');
-	var username = document.getElementById('username');
-	var password = document.getElementById('password');
+    var firstName = document.getElementById('firstName');
+    var lastName = document.getElementById('lastName');
+    var address = document.getElementById('address');
+    var phoneNumber = document.getElementById('phoneNumber');
+    var emailAddress = document.getElementById('emailAddress');
+    var username = document.getElementById('username');
+    var password = document.getElementById('password');
 
-	if (!validateRequired(firstName)) {return false;}
-        if (!validateRequired(lastName)) {return false;}
-        if (!validateRequired(address)) {return false;}
-        if (!validateRequired(phoneNumber)) {return false;}
-        if (!validateRequired(emailAddress)) {return false;}
-        if (!validateRequired(username)) {return false;}
-        if (!validateRequired(password)) {return false;}
-        
+    if (!validateRequired(firstName))
+    {
+        return false;
+    }
+    if (!validateRequired(lastName))
+    {
+        return false;
+    }
+    if (!validateRequired(address))
+    {
+        return false;
+    }
+    if (!validateRequired(phoneNumber))
+    {
+        return false;
+    }
+    if (!validateRequired(emailAddress))
+    {
+        return false;
+    }
+    if (!validateRequired(username))
+    {
+        return false;
+    }
+    if (!validateRequired(password))
+    {
+        return false;
+    }
 
+    if (!validatePhoneNumber(phoneNumber))
+    {
+        return false;
+    }
 
-	if ((fld.value.length < 5) || (fld.value.length > 15))
-	{
-		fld.style.background = 'Yellow';
-		error = "The username is the wrong length.\n";
-	}
-	else if (illegalChars.test(fld.value))
-	{
-		fld.style.background = 'Yellow';
-		error = "The username contains illegal characters.\n";
-	}
-	else
-	{
-		fld.style.background = 'White';
-	}
-	
-        return true;
+    /*
+     if ((fld.value.length < 5) || (fld.value.length > 15))
+     {
+     fld.style.background = 'Yellow';
+     error = "The username is the wrong length.\n";
+     }
+     else if (illegalChars.test(fld.value))
+     {
+     fld.style.background = 'Yellow';
+     error = "The username contains illegal characters.\n";
+     }
+     else
+     {
+     fld.style.background = 'White';
+     }
+     */
+
+    return true;
 }
 
 function validateRequired(field)
 {
-	if (field.value == "")
-	{
-		alert(field.getAttribute("name") + " is required");
-		return false;
-	}
-        else
+    if (field.value == "")
+    {
+        alert(field.getAttribute("name") + " is required");
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+
+function validatePhoneNumber(field)
+{
+    var pattern = /^02?([0-9]{8})$/; // 02dddddddd (mobile)
+    var pattern2 = /^0?([0-9]{8})$/; // 0dddddddd
+
+    if ((field.value.match(pattern)))
+    {
+        return true;
+    }
+    else
+    {
+        if ((field.value.match(pattern2)))
         {
             return true;
         }
-}
+        else
+        {
+            alert("Phone Number format should be: 0dddddddd or 02dddddddd (mobile)");
+            return false;
+        }
+    }
+}  
