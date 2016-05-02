0a61656b6d8337c46f9160c10397b07d189cd41a
patrick.cura <patrick_cura1989@yahoo.com>
2016-05-03 11:24:55 +1200

    updated code

--- project.sql
+++ project.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 26, 2016 at 12:24 AM
+-- Generation Time: May 03, 2016 at 01:24 AM
 -- Server version: 5.6.17
 -- PHP Version: 5.5.12
 
@@ -42,7 +42,15 @@ CREATE TABLE IF NOT EXISTS `cars` (
   `user_id` int(11) NOT NULL,
   PRIMARY KEY (`car_id`),
   KEY `user_id` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+--
+-- Dumping data for table `cars`
+--
+
+INSERT INTO `cars` (`car_id`, `car_name`, `fuel_type`, `make`, `model`, `year`, `price`, `url`, `description`, `user_id`) VALUES
+(4, 'BMW 116i 2013', 'Diesel', 'BMW', 'Coupe', 2013, 31900, 'http://photos.dmotorworks.com.au/at/AT5008539/506103/large/lg_506103_1.jpg', '32,500km\r\nMineralgrau Metallic 5 Door\r\n1598cc Auto\r\nPetrol', 1),
+(5, '2006 Mazda Mazda6', 'Diesel', 'Mazda', 'Coupe', 2006, 14875, 'http://photos.dmotorworks.com.au/at/AT5124597/12569/large/lg_12569_2.jpg', '84,500km\r\nWhite Hatchback\r\n2300cc 6 Speed\r\nPetrol', 1);
 
 -- --------------------------------------------------------
 
@@ -61,14 +69,20 @@ CREATE TABLE IF NOT EXISTS `users` (
   `username` varchar(15) NOT NULL,
   `password` varchar(15) NOT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`user_id`, `firstName`, `lastName`, `address`, `phoneNumber`, `email`, `username`, `password`) VALUES
-(1, 'Patrick', 'Cura', '68 Cypress Drive Maungaraki Lower Hutt New Zealand', '0212953418', 'patrick_cura1989@yahoo.com', 'patrick', 'patrick');
+(1, 'Patrick', 'Cura', '68 Cypress Drive Maungaraki Lower Hutt New Zealand', '0212953418', 'patrick_cura1989@yahoo.com', 'patrick', 'patrick'),
+(2, 'Patrick', 'Cura', '68', '0212953418', 'patrick_cura1989@yahoo.com', 'patrick', 'patrick'),
+(3, 'Patrick', 'Cura', '68', '0212953418', 'patrick_cura1989@yahoo.com', 'patrick', 'patrick'),
+(4, '', '', '', '', '', '', ''),
+(5, 'Patrick', 'Cura', '68', '0212953418', 'patrick_cura1989@yahoo.com', 'patrick', 'patrick'),
+(6, 'Patrick', 'Cura', '68', '0212953418', 'patrick_cura1989@yahoo.com', 'patrick', 'patrick'),
+(7, 'Patrick', 'Cura', '68', '0212953418', 'patrick_cura1989@yahoo.com', 'patrick', 'patrick');
 
 --
 -- Constraints for dumped tables
