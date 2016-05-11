9d4433af291758092c4d4536005fff052249d8fb
Patrick Cura <patrick.cura@soit.local>
2016-05-12 10:50:06 +1200

    updated code

--- signout.php
+++ signout.php
@@ -6,6 +6,7 @@ include_once 'header.php';
 $_SESSION["usernameSignIn"] = null;
 $_SESSION["useridSignIn"] = null;
 
+session_destroy();
 
 header("Location: http://" . HOMEURL . "/login.php"); /* Redirect browser */
 exit();
